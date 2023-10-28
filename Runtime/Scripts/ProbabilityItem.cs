05278e52892359164e04dcb75c2f552f75fc1fdd
Yurii Lunha <yurii.lunha.dev@gmail.com>
2023-10-28 16:36:43 -0600

    chore: move in

---
+++ Runtime/Scripts/ProbabilityItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace Lunha.ProbabilityTable.Runtime
+{
+    [Serializable]
+    public class ProbabilityItem
+    {
+        [SerializeField, Range(1, 100)] int m_Probability;
+
+        [SerializeField] MonoBehaviour m_ItemBehaviour;
+        [SerializeField] GameObject m_ItemObject;
+        [SerializeField] int m_ItemInt;
+        [SerializeField] float m_ItemFlt;
+
+        public int Probability
+        {
+            get => m_Probability;
+            set => m_Probability = value;
+        }
+
+        public MonoBehaviour ItemBehaviour
+        {
+            get => m_ItemBehaviour;
+            set => m_ItemBehaviour = value;
+        }
+
+        public GameObject ItemObject
+        {
+            get => m_ItemObject;
+            set => m_ItemObject = value;
+        }
+
+        public int ItemInt
+        {
+            get => m_ItemInt;
+            set => m_ItemInt = value;
+        }
+
+        public float ItemFlt
+        {
+            get => m_ItemFlt;
+            set => m_ItemFlt = value;
+        }
+    }
+}
---
+++ Runtime/Scripts/ProbabilityTable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lunha.DevKit.Extensions;
+using UnityEngine;
+
+namespace Lunha.ProbabilityTable.Runtime
+{
+    public class ProbabilityTable : MonoBehaviour
+    {
+        [SerializeField] List<ProbabilityItem> m_ProbabilityItems = new();
+
+        public List<ProbabilityItem> ProbabilityItems => m_ProbabilityItems;
+
+        /// <summary>
+        ///     Return random item based on probability
+        /// </summary>
+        /// <returns><see cref="ProbabilityItem" /> - as result you can use itemBehaviour or itemObject.</returns>
+        public ProbabilityItem GetRandom()
+        {
+            var sortedItems = m_ProbabilityItems.OrderBy(i => i.Probability);
+            var value = Random.Range(1, 101);
+            var step = 0;
+            var itemsInRange = new List<ProbabilityItem>();
+
+            foreach (var item in sortedItems)
+            {
+                if (!itemsInRange.Any()) step += item.Probability;
+
+                if (value <= step) itemsInRange.Add(item);
+            }
+
+            if (itemsInRange.Count > 1) return itemsInRange.RandomOne();
+
+            if (itemsInRange.Any()) return itemsInRange.First();
+
+            return m_ProbabilityItems.Any() ? m_ProbabilityItems.First() : null;
+        }
+    }
+}
